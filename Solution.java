d7b2cb8b2c08d590b80b7c1629e035bd67d05e85
kunjanshah6 <30865626+kunjanshah6@users.noreply.github.com>
2019-02-06 17:34:30 +0530

    Update Solution.java
    Change in Program definition

--- Solution.java
+++ Solution.java
@@ -1,10 +1,22 @@
 package projectEuler;
 
 /*
+
  * If we list all the natural numbers below 10 that are multiples of 3 or 5,
  *  we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-Find the sum of all the multiples of 3 or 5 below 1000.
+Sample Input 0
+
+2
+10
+100
+
+Sample Output 0
+23
+2318
+
+For N=10, if we list all the natural numbers below 10 that are multiples of 3 or 5 , we get  3,5,6 and 9. The sum of these multiples is 23.
+Similarly for N=100 , we get 2318.
 
 */
 
